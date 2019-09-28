2d5bf4cee310fd8ef699bb16d5196792b90813c4
KatoCode23 <Kalen23Imani@gmail.com>
2019-09-27 20:49:06 -0400

    family members

--- exercises/js-loops/script.js
+++ exercises/js-loops/script.js
@@ -1,1 +1,73 @@
-// GOOD LUCK!!!
+// GOOD LUCK!!
+var familyMembers = [
+
+    {
+        name: 'Kalen',
+        age: 37
+    },
+
+    {
+        name: 'Eleen',
+        age: 31
+    },
+
+    {
+        name: 'Kenneth',
+        age: 59
+    },
+
+    {
+        name: 'Kim',
+        age: 57
+    },
+
+    {
+        name: 'Keara',
+        age: 34
+    },
+
+    {
+        name: 'Keylee',
+        age: 29
+    },
+
+    {
+        name: 'Rylee',
+        age: 8
+    },
+
+    {
+        name: 'Kennedy',
+        age: 3
+    },
+
+    {
+        name: 'King',
+        age: 1
+    }
+];
+
+console.log(familyMembers);
+
+
+function familyData () {
+    var oldestNow = 0;
+    var oldest;
+    for (var i = 0; i < familyMembers.length; i++) {
+        console.log(familyMembers[i].name + ' is ' + familyMembers[i].age + ' years old.');
+        if(familyMembers[i].age >= 21) {
+            console.log(familyMembers[i].name + ' is old enough to drink, yay!!!');
+        }
+        if(familyMembers[i].age <= 21) {
+            console.log(familyMembers[i].name + ' is not old enough to drink, too little!');
+        }
+        else if(familyMembers[i].age > oldestNow) {
+            oldest = familyMembers[i];
+            oldestNow = familyMembers[i].age
+        }
+    }
+    return oldest.name + ' you are the oldest member of this family!';
+}
+
+var oldestMember = familyData();
+console.log(oldestMember);
